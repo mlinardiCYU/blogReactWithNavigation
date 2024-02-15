2d288afa6bc039c15093aa691d17b3a0b2d745be
Michele Linardi <mlinardi@administration.adm>
2024-02-15 14:03:33 +0100

    start all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Blog from './blog/Blog'
+import Technology from './blog/Technology'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Blog/>}/>
+        <Route path="Technology" element={<Technology/>}/>
+        {/*...*/}
+      </Routes>
+      </BrowserRouter>
+
   );
 }
 
